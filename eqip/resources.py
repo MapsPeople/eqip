12363cb32d727a27058b8b033474a25ea8e288db
Christian Heider Nielsen <mrnaah@gmail.com>
2024-06-03 13:27:56 +0200

    remove pyside2 dep

--- eqip/resources.py
+++ eqip/resources.py
@@ -3,7 +3,7 @@
 # Created by: The Resource Compiler for Qt version 5.15.2
 # WARNING! All changes made in this file will be lost!
 
-from PySide2 import QtCore
+from qgis.PyQt import QtCore
 
 qt_resource_data = b"\
 \x00\x00\x16m\
@@ -996,10 +996,13 @@ qt_resource_struct = b"\
 \x00\x00\x01\x8e\xba&\x18[\
 "
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+  QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+  QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
 
 qInitResources()
